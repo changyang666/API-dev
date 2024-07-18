aa79873b75dab51abeb349edf039bbc225d349b9
changyang666 <83751426+changyang666@users.noreply.github.com>
2024-07-18 14:48:44 +0800

    .

--- bayes_xgb_api.py
+++ bayes_xgb_api.py
@@ -94,4 +94,4 @@ async def predict(input: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
